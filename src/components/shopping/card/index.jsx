8035b741a8b51e1231fc177724e058151db7e8f6
Tobías <tobiasazcurrabi@gmail.com>
2024-10-09 20:29:47 -0300

    fdafa

--- src/components/shopping/card/index.jsx
+++ src/components/shopping/card/index.jsx
@@ -9,9 +9,9 @@ const Card = ({ name, description, price, img, path, id }) => {
 				onClick={(e) => {
 					if (img === "proximamente") e.preventDefault();
 				}}
-				className="flex items-center justify-center p-24 gap-4 shadow-md rounded-lg bg-gray-200 text-gray-700 min-w-full max-w-[400px] hover:shadow-lg transition duration-300"
+				className="flex items-center justify-center p-24 gap-4 shadow-md rounded-xl bg-gray-200 text-gray-700 min-w-full max-w-[400px] hover:shadow-lg transition duration-300"
 			>
-				<div className="flex flex-col items-center justify-center leading-normal font-antonio">
+				<div className="flex flex-col items-center justify-center leading-normal font-coolvetica">
 					<h5 className="mb-2 text-2xl font-bold tracking-tight uppercase">
 						próximamente
 					</h5>
@@ -19,17 +19,18 @@ const Card = ({ name, description, price, img, path, id }) => {
 			</Link>
 		);
 	}
-
+	// Función para capitalizar cada palabra
+	const capitalizeWords = (str) => {
+		return str.replace(/\b\w/g, (char) => char.toUpperCase());
+	};
 	return (
 		<Link
 			to={`/menu/${path}/${id}`}
-			className="flex flex-col items-center gap-4 shadow-md rounded-lg bg-gray-100 hover:shadow-lg transition duration-300 h-[340px] w-full max-w-[400px] text-black"
+			className="flex  flex-col items-center border border-black border-1 border-opacity-20 rounded-xl bg-gray-100 hover:shadow-lg transition duration-300  w-full max-w-[400px] text-black"
 		>
-			<div className="h-[150px] overflow-hidden rounded-t-lg w-full bg-gradient-to-b from-gray-300 to-red-400">
+			<div className="h-[150px] overflow-hidden rounded-t-xl w-full bg-gradient-to-b from-gray-300 to-red-400">
 				<img
-					className={`object-cover w-full h-full ${
-						img === "bURGERS.png" ? "pb-4" : ""
-					}`}
+					className="object-cover w-full h-full"
 					src={`/menu/${img}`}
 					alt={img}
 					style={{
@@ -37,12 +38,12 @@ const Card = ({ name, description, price, img, path, id }) => {
 					}}
 				/>
 			</div>
-			<div className="flex flex-col items-center justify-between leading-normal font-antonio px-2 text-center">
-				<h5 className="mb-2 text-xl font-bold tracking-tight">
-					{name.toUpperCase()}
+			<div className="flex p-4 flex-col items-center justify-between leading-normal font-coolvetica  text-center">
+				<h5 className="mb-1 text-2xl  font-bold tracking-tight">
+					{capitalizeWords(name)}
 				</h5>
-				<p className="mb-2 text-sm text-gray-600">{description}</p>
-				<span className="font-bold text-lg text-black">
+				<p className="mb-1 text-xs text-gray-600">{description}</p>
+				<span className="font-bold text-2xl mb-3 text-black">
 					{currencyFormat(price)}
 				</span>
 			</div>
