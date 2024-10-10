a1a5a5ae3edf5c95c6487c6963c27e71b83dbd7a
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 12:24:52 -0300

    dfasfa

--- src/components/shopping/FloatingCart.jsx
+++ src/components/shopping/FloatingCart.jsx
@@ -4,7 +4,7 @@ import currencyFormat from "../../helpers/currencyFormat";
 
 const FloatingCart = ({ totalQuantity, cart }) => {
 	return (
-		<div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg p-4 flex items-center justify-between">
+		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] p-4 flex items-center justify-between">
 			<span className="text-black text-lg font-bold">
 				{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"} -{" "}
 				{currencyFormat(
@@ -13,7 +13,7 @@ const FloatingCart = ({ totalQuantity, cart }) => {
 			</span>
 			<Link
 				to="/carrito"
-				className="bg-red-600 text-white px-6 py-3 rounded-lg font-bold text-lg"
+				className="bg-red-600 text-gray-100 px-6 py-3 rounded-lg font-bold text-lg"
 			>
 				Ver carrito
 			</Link>
