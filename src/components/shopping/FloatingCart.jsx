89558fb9e27301e4bc30e4dd7764e5a9a6294a65
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 12:28:33 -0300

    fasfa

--- src/components/shopping/FloatingCart.jsx
+++ src/components/shopping/FloatingCart.jsx
@@ -4,16 +4,20 @@ import currencyFormat from "../../helpers/currencyFormat";
 
 const FloatingCart = ({ totalQuantity, cart }) => {
 	return (
-		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] p-4 flex items-center justify-between">
-			<span className="text-black text-lg font-bold">
-				{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"} -{" "}
-				{currencyFormat(
-					cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
-				)}
-			</span>
+		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3 px-4 flex items-center justify-between font-coolvetica w-full">
+			<div className="flex flex-col items-center w-1/3">
+				<p className="">
+					{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"}
+				</p>
+				<p>
+					{currencyFormat(
+						cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
+					)}
+				</p>
+			</div>
 			<Link
 				to="/carrito"
-				className="bg-red-600 text-gray-100 px-6 py-3 rounded-lg font-bold text-lg"
+				className="bg-red-600 w-2/3 text-center text-gray-100 h-10 text-xs px-6 py-3 rounded-lg font-bold "
 			>
 				Ver carrito
 			</Link>
