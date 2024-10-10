4d757857cae64d851a0c452b99eac4ca67548f4d
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 12:39:25 -0300

    dsfgs

--- src/components/shopping/FloatingCart.jsx
+++ src/components/shopping/FloatingCart.jsx
@@ -6,7 +6,7 @@ const FloatingCart = ({ totalQuantity, cart }) => {
 	return (
 		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3 px-4 flex flex-row items-center justify-between gap-4 font-coolvetica w-full z-50">
 			<div className="flex flex-col items-center">
-				<p className=" text-xs font-bold ">
+				<p className=" text-xs font-medium ">
 					{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"}
 				</p>
 				<p className="text-4xl font-bold mt-[-5px]">
