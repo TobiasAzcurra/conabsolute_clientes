4e98b1529890b10572801cf03ddc2b33410f5000
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 13:17:26 -0300

    fdasfa

--- src/components/shopping/card/quickAddToCart.jsx
+++ src/components/shopping/card/quickAddToCart.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { addItem } from "../../../redux/cart/cartSlice";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const QuickAddToCart = ({ product }) => {
 	const dispatch = useDispatch();
@@ -40,25 +41,30 @@ const QuickAddToCart = ({ product }) => {
 	};
 
 	return (
-		<div className="  pt-0.5 w-[35px] h-[35px] text-center cursor-pointer flex items-center justify-center relative">
+		<div className="pt-0.5 w-[35px] h-[35px] text-center cursor-pointer flex items-center justify-center relative">
 			{isEditing ? (
-				<div className="flex items-center absolute right-0 top-0 flex-row w-[100px]  rounded-lg font-black border border-black border-opacity-20 bg-gray-100">
+				<motion.div
+					initial={{ width: 35 }}
+					animate={{ width: 100 }}
+					transition={{ duration: 0.3 }}
+					className="flex items-center absolute right-0 top-0 flex-row w-[100px] rounded-lg font-black border border-black border-opacity-20 bg-gray-100"
+				>
 					<div
-						className=" text-black font-bold text-center items-center flex justify-center w-[35px] h-[35px]"
+						className="text-black font-bold text-center items-center flex justify-center w-[35px] h-[35px] cursor-pointer"
 						onClick={handleDecrement}
 					>
 						-
 					</div>
-					<span className="font-antonio font-bold text-center items-center flex justify-center  w-[35px] h-[35px]">
+					<span className="font-antonio font-bold text-center items-center flex justify-center w-[35px] h-[35px]">
 						{quantity}
 					</span>
 					<div
-						className=" text-block font-bold  text-center items-center flex justify-center w-[35px] h-[35px]"
+						className="text-black font-bold text-center items-center flex justify-center w-[35px] h-[35px] cursor-pointer"
 						onClick={handleIncrement}
 					>
 						+
 					</div>
-				</div>
+				</motion.div>
 			) : (
 				<div
 					className="bg-gray-100 rounded-lg font-black border border-black border-opacity-20 pt-0.5 w-[35px] h-[35px] text-center cursor-pointer"
