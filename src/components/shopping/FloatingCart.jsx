98b12058a647c57ff304ab001afdeceafa1b46db
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 12:31:32 -0300

    fadfdad

--- src/components/shopping/FloatingCart.jsx
+++ src/components/shopping/FloatingCart.jsx
@@ -4,12 +4,12 @@ import currencyFormat from "../../helpers/currencyFormat";
 
 const FloatingCart = ({ totalQuantity, cart }) => {
 	return (
-		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3 px-4 flex items-center justify-between font-coolvetica w-full">
-			<div className="flex flex-col items-center w-1/3">
-				<p className="">
+		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3 px-4 flex flex-row items-center justify-between font-coolvetica w-full">
+			<div className="flex flex-col items-center ">
+				<p className=" text-xs font-bold ">
 					{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"}
 				</p>
-				<p>
+				<p className="text-4xl font-bold mt-[-5px]">
 					{currencyFormat(
 						cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
 					)}
