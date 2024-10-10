818f3739c4f0c5663d1f4bf3d001457840826588
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 12:38:36 -0300

    fdgs

--- src/components/shopping/FloatingCart.jsx
+++ src/components/shopping/FloatingCart.jsx
@@ -4,7 +4,7 @@ import currencyFormat from "../../helpers/currencyFormat";
 
 const FloatingCart = ({ totalQuantity, cart }) => {
 	return (
-		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3 px-4 flex flex-row items-center justify-between font-coolvetica w-full z-50">
+		<div className="fixed bottom-0 left-0 right-0 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.3),0px_-4px_6px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3 px-4 flex flex-row items-center justify-between gap-4 font-coolvetica w-full z-50">
 			<div className="flex flex-col items-center">
 				<p className=" text-xs font-bold ">
 					{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"}
@@ -17,7 +17,7 @@ const FloatingCart = ({ totalQuantity, cart }) => {
 			</div>
 			<Link
 				to="/carrito"
-				className="bg-red-600 w-2/3 text-center text-gray-100 h-10 text-xs px-6 py-3 rounded-lg font-bold "
+				className="bg-red-600 w-2/3 text-center items-center flex justify-center text-gray-100 h-12 text-sm px-6 py-3 rounded-lg font-bold "
 			>
 				Ver carrito
 			</Link>
