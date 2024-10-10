6d298519b1eeb87352596803d5237949518c174b
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 13:09:39 -0300

    fdafads

--- src/components/shopping/card/quickAddToCart.jsx
+++ src/components/shopping/card/quickAddToCart.jsx
@@ -1,30 +1,72 @@
 import { useDispatch } from "react-redux";
 import { addItem } from "../../../redux/cart/cartSlice";
+import { useState } from "react";
 
 const QuickAddToCart = ({ product }) => {
 	const dispatch = useDispatch();
+	const [quantity, setQuantity] = useState(1);
+	const [isAdding, setIsAdding] = useState(false);
+	const [isEditing, setIsEditing] = useState(false);
 
-	const addToCart = () => {
-		// Crear el objeto del producto con los detalles necesarios
-		const burgerObject = {
-			name: product.name,
-			price: product.price,
-			img: product.img,
-			toppings: [], // Asumiendo que no hay toppings en quick add
-			quantity: 1,
-			category: product.category || "default", // Ajusta según tu lógica
-		};
+	const handleIncrement = () => {
+		setQuantity((prevQuantity) => prevQuantity + 1);
+	};
 
-		// Despachar la acción para agregar el producto al carrito
-		dispatch(addItem(burgerObject));
+	const handleDecrement = () => {
+		if (quantity > 1) {
+			setQuantity((prevQuantity) => prevQuantity - 1);
+		}
+	};
+
+	const startAddingProcess = () => {
+		setIsEditing(true);
+		setIsAdding(true);
+		setTimeout(() => {
+			if (quantity >= 1) {
+				const burgerObject = {
+					name: product.name,
+					price: product.price,
+					img: product.img,
+					toppings: [],
+					quantity,
+					category: product.category || "default",
+				};
+
+				dispatch(addItem(burgerObject));
+			}
+			setIsAdding(false);
+			setIsEditing(false);
+		}, 5000000000);
 	};
 
 	return (
-		<div
-			className="bg-gray-100 rounded-lg font-black border border-black border-opacity-20 pt-0.5 w-[35px] h-[35px] text-center cursor-pointer"
-			onClick={addToCart}
-		>
-			+
+		<div className="bg-gray-100 rounded-lg font-black border border-black border-opacity-20 pt-0.5 w-[35px] h-[35px] text-center cursor-pointer flex items-center justify-center">
+			{isEditing ? (
+				<div className="flex items-center flex-row w-[100px]  rounded-lg font-black border border-black border-opacity-20 bg-gray-100">
+					<div
+						className=" text-black font-bold text-center items-center flex justify-center w-[35px] h-[35px]"
+						onClick={handleDecrement}
+					>
+						-
+					</div>
+					<span className="font-antonio font-bold text-center items-center flex justify-center  w-[35px] h-[35px]">
+						{quantity}
+					</span>
+					<div
+						className=" text-block font-bold  text-center items-center flex justify-center w-[35px] h-[35px]"
+						onClick={handleIncrement}
+					>
+						+
+					</div>
+				</div>
+			) : (
+				<div
+					className="bg-gray-100 rounded-lg font-black border border-black border-opacity-20 pt-0.5 w-[35px] h-[35px] text-center cursor-pointer"
+					onClick={startAddingProcess}
+				>
+					+
+				</div>
+			)}
 		</div>
 	);
 };
