fe00b5d1a49d2c32b81ae5c97d56a67dc871fed5
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 17:37:11 -0300

    fdafa

--- src/components/shopping/card/quickAddToCart.jsx
+++ src/components/shopping/card/quickAddToCart.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
-import { addItem } from "../../../redux/cart/cartSlice";
-import { useState } from "react";
+import { addItem, updateItemQuantity } from "../../../redux/cart/cartSlice";
+import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 
 const QuickAddToCart = ({ product }) => {
@@ -9,39 +9,72 @@ const QuickAddToCart = ({ product }) => {
 	const [quantity, setQuantity] = useState(1);
 	const [isAdding, setIsAdding] = useState(false);
 	const [isEditing, setIsEditing] = useState(false);
+	const quantityRef = useRef(quantity);
 
 	const cartItem = cart.find((item) => item.name === product.name);
 	const cartQuantity = cartItem ? cartItem.quantity : 0;
 
 	const handleIncrement = () => {
-		setQuantity((prevQuantity) => prevQuantity + 1);
+		console.log("Incrementing quantity");
+		setQuantity((prevQuantity) => {
+			const newQuantity = prevQuantity + 1;
+			quantityRef.current = newQuantity;
+			return newQuantity;
+		});
 	};
 
 	const handleDecrement = () => {
 		if (quantity > 1) {
-			setQuantity((prevQuantity) => prevQuantity - 1);
+			console.log("Decrementing quantity");
+			setQuantity((prevQuantity) => {
+				const newQuantity = prevQuantity - 1;
+				quantityRef.current = newQuantity;
+				return newQuantity;
+			});
 		}
 	};
 
 	const startAddingProcess = () => {
+		console.log("Starting adding process");
 		setIsEditing(true);
 		setIsAdding(true);
 		setTimeout(() => {
-			if (quantity >= 1) {
-				const burgerObject = {
-					name: product.name,
-					price: product.price,
-					img: product.img,
-					toppings: [],
-					quantity,
-					category: product.category || "default",
-				};
+			if (quantityRef.current >= 1) {
+				if (cartItem) {
+					console.log("Updating item quantity in cart", {
+						name: product.name,
+						quantity: quantityRef.current,
+					});
+					dispatch(
+						updateItemQuantity({
+							name: product.name,
+							toppings: [],
+							quantity: quantityRef.current,
+						})
+					);
+				} else {
+					console.log("Adding item to cart", {
+						name: product.name,
+						quantity: quantityRef.current,
+					});
+					const burgerObject = {
+						name: product.name,
+						price: product.price,
+						img: product.img,
+						toppings: [],
+						quantity: quantityRef.current,
+						category: product.category || "default",
+					};
 
-				dispatch(addItem(burgerObject));
+					dispatch(addItem(burgerObject));
+				}
+				console.log("Current cart state:", cart);
 			}
 			setIsAdding(false);
+			console.log("Finished adding item");
 			setTimeout(() => {
 				setIsEditing(false);
+				console.log("Finished editing");
 			}, 300); // Añadimos un pequeño retraso para completar la animación de cierre
 		}, 2000);
 	};
--- src/redux/cart/cartSlice.js
+++ src/redux/cart/cartSlice.js
@@ -1,85 +1,103 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const getTotal = (cart) => {
-  let total = 0;
-  let totalToppings = 0;
-  cart.forEach(({ price, quantity, toppings }) => {
-    total += price * quantity;
-    toppings.forEach(({ price }) => {
-      totalToppings += price * quantity;
-    });
-  });
-  return total + totalToppings;
+	let total = 0;
+	let totalToppings = 0;
+	cart.forEach(({ price, quantity, toppings }) => {
+		total += price * quantity;
+		toppings.forEach(({ price }) => {
+			totalToppings += price * quantity;
+		});
+	});
+	return total + totalToppings;
 };
 
 export const cartSlices = createSlice({
-  name: 'products',
-  initialState: {
-    cart: [],
-    lastCart: [],
-    total: 0,
-  },
+	name: "products",
+	initialState: {
+		cart: [],
+		lastCart: [],
+		total: 0,
+	},
 
-  reducers: {
-    addItem: (state, action) => {
-      const item = action.payload;
-      const existingItemIndex = state.cart.findIndex(
-        (cartItem) =>
-          cartItem.name === item.name &&
-          JSON.stringify(cartItem.toppings) === JSON.stringify(item.toppings)
-      );
+	reducers: {
+		addItem: (state, action) => {
+			const item = action.payload;
+			const existingItemIndex = state.cart.findIndex(
+				(cartItem) =>
+					cartItem.name === item.name &&
+					JSON.stringify(cartItem.toppings) === JSON.stringify(item.toppings)
+			);
 
-      if (existingItemIndex >= 0) {
-        state.cart[existingItemIndex].quantity += item.quantity;
-      } else {
-        state.cart = [...state.cart, item];
-      }
+			if (existingItemIndex >= 0) {
+				// Sumar la cantidad a la existente (acumulativa)
+				state.cart[existingItemIndex].quantity += item.quantity;
+			} else {
+				// Agregar un nuevo artículo
+				state.cart = [...state.cart, item];
+			}
 
-      state.total = getTotal(state.cart);
-    },
+			state.total = getTotal(state.cart);
+		},
 
-    removeOneItem: (state, action) => {
-      const index = action.payload;
-      state.cart[index].quantity -= 1;
-      state.total = getTotal(state.cart);
-    },
+		updateItemQuantity: (state, action) => {
+			const { name, toppings, quantity } = action.payload;
+			const existingItemIndex = state.cart.findIndex(
+				(cartItem) =>
+					cartItem.name === name &&
+					JSON.stringify(cartItem.toppings) === JSON.stringify(toppings)
+			);
 
-    addOneItem: (state, action) => {
-      const index = action.payload;
-      state.cart[index].quantity += 1;
-      state.total = getTotal(state.cart);
-    },
+			if (existingItemIndex >= 0) {
+				// Reemplazar la cantidad existente con la nueva cantidad proporcionada
+				state.cart[existingItemIndex].quantity = quantity;
+				state.total = getTotal(state.cart);
+			}
+		},
 
-    removeItem: (state, action) => {
-      const index = action.payload;
-      const itemToRemove = state.cart[index];
-      state.cart.splice(index, 1);
-      state.total = getTotal(state.cart);
-    },
+		removeOneItem: (state, action) => {
+			const index = action.payload;
+			state.cart[index].quantity -= 1;
+			state.total = getTotal(state.cart);
+		},
 
-    clearCart: (state) => {
-      state.cart = [];
-      state.total = 0;
-    },
+		addOneItem: (state, action) => {
+			const index = action.payload;
+			state.cart[index].quantity += 1;
+			state.total = getTotal(state.cart);
+		},
 
-    addLastCart: (state) => {
-      state.lastCart = state.cart;
-    },
+		removeItem: (state, action) => {
+			const index = action.payload;
+			state.cart.splice(index, 1);
+			state.total = getTotal(state.cart);
+		},
 
-    changeLastCart: (state) => {
-      state.cart = state.lastCart;
-      state.total = getTotal(state.lastCart);
-    },
-  },
+		clearCart: (state) => {
+			state.cart = [];
+			state.total = 0;
+		},
+
+		addLastCart: (state) => {
+			state.lastCart = state.cart;
+		},
+
+		changeLastCart: (state) => {
+			state.cart = state.lastCart;
+			state.total = getTotal(state.lastCart);
+		},
+	},
 });
 
 export const {
-  addItem,
-  removeOneItem,
-  addOneItem,
-  clearCart,
-  removeItem,
-  addLastCart,
-  changeLastCart,
+	addItem,
+	updateItemQuantity,
+	removeOneItem,
+	addOneItem,
+	clearCart,
+	removeItem,
+	addLastCart,
+	changeLastCart,
 } = cartSlices.actions;
+
 export default cartSlices.reducer;
