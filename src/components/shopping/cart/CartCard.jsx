9cc20b518fa77f6c7ec54575a2d64305ee4e2cdf
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 23:02:11 -0300

    fdafa

--- src/components/shopping/cart/CartCard.jsx
+++ src/components/shopping/cart/CartCard.jsx
@@ -18,7 +18,7 @@ const CartCard = ({
 				<img
 					src={img ? `/menu/${img}` : getDefaultImage(item)}
 					alt={name}
-					className=" h-[270px] object-cover"
+					className=" h-[300px] object-cover"
 				/>
 			</div>
 
