ab352a8f013ec17819959fca458320756f0a46e4
Tobías <tobiasazcurrabi@gmail.com>
2024-10-10 23:52:26 -0300

    fadsfa

--- src/components/shopping/cart/index.jsx
+++ src/components/shopping/cart/index.jsx
@@ -15,6 +15,7 @@ import burgers from "../../../assets/burgers-v1.json";
 import papas from "../../../assets/papas-v1.json";
 import drinks from "../../../assets/drinks-v1.json";
 import CartCard from "./CartCard";
+import arrow from "../../../assets/arrowIcon.png";
 
 // Agregar la categoría a cada producto
 const burgersArray = Object.values(burgers).map((product) => ({
@@ -150,23 +151,26 @@ const CartItems = () => {
 			<div className="flex justify-center flex-col mt-6 items-center px-4">
 				<p className="text-2xl font-bold mb-2">Metodo de pago</p>
 				<div className="w-full items-center    rounded-xl border-2 border-black ">
-					<div className="flex flex-row gap-2 pl-4 h-10 items-center border border-black border-opacity-20">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							className="h-6"
-						>
-							<path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
-							<path
-								fill-rule="evenodd"
-								d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V9.75a.75.75 0 0 0-.75-.75h-.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z"
-								clip-rule="evenodd"
-							/>
-							<path d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z" />
-						</svg>
+					<div className="flex flex-row justify-between px-4 h-10 items-center border border-black border-opacity-20">
+						<div className="flex flex-row gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="h-6"
+							>
+								<path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+								<path
+									fill-rule="evenodd"
+									d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V9.75a.75.75 0 0 0-.75-.75h-.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z"
+									clip-rule="evenodd"
+								/>
+								<path d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z" />
+							</svg>
 
-						<p className=" text-opacity-20 text-black">Efectivo</p>
+							<p className=" text-opacity-20 text-black">Efectivo</p>
+						</div>
+						<img src={arrow} className="h-2" alt="" />
 					</div>
 					<div className="flex flex-row gap-2 pl-4 h-10 items-center">
 						<svg
