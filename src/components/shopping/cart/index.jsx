24ed37c7967feebe5fdfac7e3052f3a6ab44ff8c
Tobías <tobiasazcurrabi@gmail.com>
2024-10-11 00:15:45 -0300

    sdfa

--- src/components/shopping/cart/index.jsx
+++ src/components/shopping/cart/index.jsx
@@ -146,9 +146,47 @@ const CartItems = () => {
 					</div>
 				</div>
 			</div>
-			<div className="flex justify-center flex-col mt-3.5 items-center">
-				<p className="text-2xl font-bold">Detalle de la entrega</p>
-				<div>Mapa + form</div>
+			<div className="flex justify-center flex-col mt-3.5 items-center px-4">
+				<p className="text-2xl font-bold mb-2">Detalle de la entrega</p>
+				<div className="w-full items-center    rounded-xl border-2 border-black ">
+					<div className="flex flex-row justify-between px-3 h-10 items-center border border-black border-opacity-20">
+						<div className="flex flex-row gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="h-6"
+							>
+								<path d="M12 7.5a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
+								<path
+									fill-rule="evenodd"
+									d="M1.5 4.875C1.5 3.839 2.34 3 3.375 3h17.25c1.035 0 1.875.84 1.875 1.875v9.75c0 1.036-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 14.625v-9.75ZM8.25 9.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM18.75 9a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V9.75a.75.75 0 0 0-.75-.75h-.008ZM4.5 9.75A.75.75 0 0 1 5.25 9h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H5.25a.75.75 0 0 1-.75-.75V9.75Z"
+									clip-rule="evenodd"
+								/>
+								<path d="M2.25 18a.75.75 0 0 0 0 1.5c5.4 0 10.63.722 15.6 2.075 1.19.324 2.4-.558 2.4-1.82V18.75a.75.75 0 0 0-.75-.75H2.25Z" />
+							</svg>
+
+							<p className=" text-opacity-20 text-black">Efectivo</p>
+						</div>
+						<img src={arrow} className="h-2" alt="" />
+					</div>
+					<div className="flex flex-row gap-2 pl-3 h-10 items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							class="h-6"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+
+						<p className=" text-opacity-20 text-black">¿Tenes un cupon?</p>
+					</div>
+				</div>
 			</div>
 			<div className="flex justify-center flex-col mt-6 items-center px-4">
 				<p className="text-2xl font-bold mb-2">Metodo de pago</p>
