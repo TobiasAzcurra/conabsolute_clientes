c3c8a8ed8b0a405e90eddc64b46ea7397462eab8
Tobías <tobiasazcurrabi@gmail.com>
2024-10-14 15:04:22 -0300

    fdsafasd

--- src/components/back/index.jsx
+++ src/components/back/index.jsx
@@ -1,20 +1,22 @@
-import { Link, useNavigate } from 'react-router-dom';
-import arrowBack from '../../assets/arrowBack.png';
+import { Link, useNavigate } from "react-router-dom";
+import arrow from "../../assets/arrowIcon.png";
 
-const ArrowBack = ({ category = 'Volver' }) => {
-  const navigate = useNavigate();
+const ArrowBack = () => {
+	const navigate = useNavigate();
 
-  return (
-    <Link
-      className="flex items-center text-xs hover:cursor-pointer font-antonio  sm:font-bold  font-light text-black relative group transition-all duration-300 ease-in-out gap-2"
-      onClick={() => navigate(-1)}
-    >
-      <img src={arrowBack} className="h-1" />
-      <span className="bg-left-bottom bg-gradient-to-r from-black py-1 to-black bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-        {category.toUpperCase()}
-      </span>
-    </Link>
-  );
+	const handleGoBack = () => {
+		navigate(-1); // Navega hacia la página previa
+	};
+
+	return (
+		<div
+			className="absolute top-2 left-4 flex flex-row items-center gap-2 cursor-pointer"
+			onClick={handleGoBack} // Añadimos el evento onClick aquí
+		>
+			<img src={arrow} className="h-2 rotate-180" alt="" />
+			<p className="font-coolvetica font-bold">Volver</p>
+		</div>
+	);
 };
 
 export default ArrowBack;
