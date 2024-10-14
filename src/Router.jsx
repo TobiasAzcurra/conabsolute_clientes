9f7cee31f1e12c0afa125de1180c53422a7983ae
Tobías <tobiasazcurrabi@gmail.com>
2024-10-14 19:27:05 -0300

    fadsfa

--- src/Router.jsx
+++ src/Router.jsx
@@ -51,24 +51,24 @@ const AppRouter = () => {
 			{/* Mostrar NavMenu y Carrusel solo en las rutas específicas */}
 			{shouldShowCarruselAndNavMenu && (
 				<div className="relative mb-[90px]">
-					<div className="flex justify-center">
-						<p className="bg-gray-100 border  border-black border-opacity-20 shadow-black  w-5/6 h-10 flex items-center justify-center absolute z-50 top-4 font-coolvetica rounded-lg ">
+					<div className="flex justify-center w-full ">
+						<div className="bg-gray-100 border border-black border-opacity-20 shadow-black h-10 flex items-center justify-center absolute z-50 top-4 font-coolvetica rounded-3xl px-4 left-4 right-4">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="currentColor"
-								className="ml-[-10px] mr-[10px] h-6"
+								className="ml-[-10px] opacity-20 mr-[10px] h-6"
 							>
 								<path
-									fill-rule="evenodd"
+									fillRule="evenodd"
 									d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-									clip-rule="evenodd"
+									clipRule="evenodd"
 								/>
 							</svg>
-							<p className="text-opacity-20 text-black">
-								Busca tu pedido con tu numero de cel:
-							</p>
-						</p>
+							<span className="text-opacity-20 text-black">
+								Busca tu pedido con tu número de cel:
+							</span>
+						</div>
 					</div>
 					<Carrusel />
 					<div className="top-[215px] inset-0 absolute">
@@ -77,7 +77,7 @@ const AppRouter = () => {
 				</div>
 			)}
 			<Routes>
-				{/* Mostrar `RouterMenu` solo en la ruta raíz */}
+				{/* Mostrar RouterMenu solo en la ruta raíz */}
 				<Route path="/" element={<RouterMenu />} />
 
 				{/* Rutas del menú */}
