c27aeefaa3eb9ded271ddf67cf8c3c082c3fabe6
Tobías <tobiasazcurrabi@gmail.com>
2024-10-14 21:46:29 -0300

    fadfa

--- src/components/shopping/cart/index.jsx
+++ src/components/shopping/cart/index.jsx
@@ -102,7 +102,7 @@ const CartItems = () => {
 	return (
 		<div className="flex flex-col font-coolvetica">
 			<ArrowBack />
-			<div className="flex justify-center flex-col mt-6 items-center">
+			<div className="flex justify-center flex-col mt-10 items-center">
 				<p className="text-2xl font-bold">Tu carrito</p>
 				<div className="flex flex-col gap-2 w-full mt-2 px-4">
 					{cart.map((item, index) => (
