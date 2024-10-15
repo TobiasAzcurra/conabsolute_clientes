5e0fd36de44685e7b1c1d01fd9a181ff013c2752
Tobías <tobiasazcurrabi@gmail.com>
2024-10-15 05:03:26 -0300

    gdsfgs

--- src/components/back/index.jsx
+++ src/components/back/index.jsx
@@ -1,17 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import arrow from "../../assets/arrowIcon.png";
 
 const ArrowBack = () => {
 	const navigate = useNavigate();
+	const location = useLocation();
 
 	const handleGoBack = () => {
-		navigate(-1); // Navega hacia la página previa
+		if (location.pathname === "/pedido") {
+			navigate("/"); // Si estás en "/pedido", navega hacia "/menu"
+		} else {
+			navigate(-1); // De lo contrario, navega hacia la página previa
+		}
 	};
 
 	return (
 		<div
 			className="absolute top-2 left-4 flex flex-row items-center gap-2 cursor-pointer"
-			onClick={handleGoBack} // Añadimos el evento onClick aquí
+			onClick={handleGoBack}
 		>
 			<img src={arrow} className="h-2 rotate-180" alt="" />
 			<p className="font-coolvetica font-bold">Volver</p>
