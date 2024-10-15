537b4f2620108fb46e5c3ddf27dd66d604b9fa68
tomy <tomychi352@gmail.com>
2024-10-15 20:10:02 -0300

    firebase functions

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,19 +1,21 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
-import cart from '../redux/cart/cartSlice';
+import cart from "../redux/cart/cartSlice";
+import user from "../redux/user/userSlice";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  whitelist: ['cartState'],
+  whitelist: ["cartState", "userState"],
 };
 
 const rootReducer = combineReducers({
   cartState: cart,
+  userState: user, // Agrega el estado de usuario autenticado
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
