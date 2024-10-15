3ec17ae453427a77ae702035e536bdce69072b86
Tobías <tobiasazcurrabi@gmail.com>
2024-10-15 03:01:37 -0300

    fadsfasd

--- src/components/shopping/cart/index.jsx
+++ src/components/shopping/cart/index.jsx
@@ -201,6 +201,26 @@ const CartItems = () => {
 							</p>
 						</div>
 					</div>
+					<div className="flex flex-row justify-between px-3 h-10 items-center border border-black border-opacity-20">
+						<div className="flex flex-row gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="h-6"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+
+							<p className=" text-opacity-20 text-black">
+								¿Queres reservar para mas tarde?
+							</p>
+						</div>
+					</div>
 					<div className="flex flex-row gap-2 pl-3 h-10 items-center">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
