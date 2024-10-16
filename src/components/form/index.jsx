30b3fd92c7b5b3e871fa17f282f17bf6d4740267
Tobías <tobiasazcurrabi@gmail.com>
2024-10-16 15:52:25 -0300

    adfasdf

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -418,9 +418,11 @@ const FormCustom = ({ cart, total }) => {
 											{couponCodes.map((coupon, index) => (
 												<div
 													key={index}
-													className="flex flex-col h-10 w-full gap-2"
+													className={`flex flex-col w-full gap-2 transition-all duration-300 ${
+														voucherStatus[index] ? "" : ""
+													}`}
 												>
-													<div className="flex flex-row gap-2 px-3  items-center">
+													<div className="flex flex-row gap-2 px-3 items-center">
 														<svg
 															xmlns="http://www.w3.org/2000/svg"
 															viewBox="0 0 24 24"
@@ -428,9 +430,9 @@ const FormCustom = ({ cart, total }) => {
 															className="h-6"
 														>
 															<path
-																fill-rule="evenodd"
+																fillRule="evenodd"
 																d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
-																clip-rule="evenodd"
+																clipRule="evenodd"
 															/>
 														</svg>
 
@@ -461,7 +463,7 @@ const FormCustom = ({ cart, total }) => {
 
 													{/* Mostrar el mensaje de código válido o inválido */}
 													{voucherStatus[index] && (
-														<div className="flex flex-row  h-10 justify-between px-3  items-start ">
+														<div className="flex flex-row h-10 justify-between px-3 items-start">
 															<div className="flex flex-row items-center gap-2">
 																<svg
 																	xmlns="http://www.w3.org/2000/svg"
@@ -470,13 +472,13 @@ const FormCustom = ({ cart, total }) => {
 																	className="h-6"
 																>
 																	<path
-																		fill-rule="evenodd"
+																		fillRule="evenodd"
 																		d="M1.5 6.375c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v3.026a.75.75 0 0 1-.375.65 2.249 2.249 0 0 0 0 3.898.75.75 0 0 1 .375.65v3.026c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 17.625v-3.026a.75.75 0 0 1 .374-.65 2.249 2.249 0 0 0 0-3.898.75.75 0 0 1-.374-.65V6.375Zm15-1.125a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0V6a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0v.75a.75.75 0 0 0 1.5 0v-.75Zm-.75 3a.75.75 0 0 1 .75.75v.75a.75.75 0 0 1-1.5 0v-.75a.75.75 0 0 1 .75-.75Zm.75 4.5a.75.75 0 0 0-1.5 0V18a.75.75 0 0 0 1.5 0v-.75ZM6 12a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H6.75A.75.75 0 0 1 6 12Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z"
-																		clip-rule="evenodd"
+																		clipRule="evenodd"
 																	/>
 																</svg>
-																<p className="bg-transparent  px-0 text-opacity-20   w-full ">
-																	Código válido, ¿Tenes alguno más?
+																<p className="bg-transparent px-0 text-opacity-20 w-full">
+																	Código válido, ¿Tienes alguno más?
 																</p>
 															</div>
 														</div>
