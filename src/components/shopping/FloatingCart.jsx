239aadeaccc9aead306174c22d290bfee336e9af
Tobías <tobiasazcurrabi@gmail.com>
2024-10-17 19:54:44 -0300

    gdsfgs

--- src/components/shopping/FloatingCart.jsx
+++ src/components/shopping/FloatingCart.jsx
@@ -21,7 +21,7 @@ const FloatingCart = ({ totalQuantity, cart }) => {
 	}, 0);
 
 	return (
-		<div className="fixed bottom-0 left-0 right-0 h-20 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.1),0px_-4px_6px_-2px_rgba(0,0,0,0.1)]  px-4 flex flex-row items-center justify-between gap-4 font-coolvetica w-full z-50">
+		<div className="fixed bottom-0 left-0 right-0 h-20 bg-gray-100 shadow-[0px_-10px_15px_-3px_rgba(0,0,0,0.1),0px_-4px_6px_-2px_rgba(0,0,0,0.1)]  px-4 flex flex-row items-center justify-between md:justify-center gap-4 font-coolvetica w-full z-50">
 			<div className="flex flex-col items-center">
 				<p className=" text-xs font-medium ">
 					{totalQuantity} {totalQuantity === 1 ? "producto" : "productos"}
@@ -32,7 +32,7 @@ const FloatingCart = ({ totalQuantity, cart }) => {
 			</div>
 			<Link
 				to="/carrito"
-				className="bg-red-main w-full items-center flex justify-center  h-12  px-6  rounded-3xl  "
+				className="bg-red-main w-full items-center flex justify-center md:w-[500px] h-12  px-6  rounded-3xl  "
 			>
 				<p className="text-gray-100 mb-1 text-2xl font-bold text-center">
 					Ver pedido
