7f06c500285ead86174e27f52059f558dd7ca455
Tobías <tobiasazcurrabi@gmail.com>
2024-10-18 16:58:20 -0300

    gfds

--- src/Router.jsx
+++ src/Router.jsx
@@ -37,6 +37,9 @@ const AppRouter = () => {
 	// Agrega el estado selectedItem
 	const [selectedItem, setSelectedItem] = useState("");
 
+	// Estado para controlar la visibilidad de la explicación
+	const [showExplanation, setShowExplanation] = useState(false);
+
 	// Función para manejar el clic en un item
 	const handleItemClick = (name) => {
 		setSelectedItem(name);
@@ -81,9 +84,10 @@ const AppRouter = () => {
 			{/* Mostrar NavMenu y Carrusel solo en las rutas específicas */}
 			{shouldShowCarruselAndNavMenu && (
 				<div className="relative mb-[90px]">
-					<div className="flex justify-center  w-full">
+					<div className="flex justify-center w-full">
 						{/* Search by phonenumber */}
 						<div className="bg-gray-100 md:w-[500px] shadow-black h-10 flex items-center justify-center absolute z-50 top-4 font-coolvetica rounded-full px-4 left-4 right-4 md:left-auto md:right-auto">
+							{/* Ícono de búsqueda */}
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
@@ -99,6 +103,7 @@ const AppRouter = () => {
 								/>
 							</svg>
 
+							{/* Input para el número de teléfono */}
 							<input
 								type="text"
 								value={phoneNumber}
@@ -107,27 +112,35 @@ const AppRouter = () => {
 								placeholder="Busca tu pedido con tu número de cel"
 								className="text-opacity-60 font-coolvetica text-black bg-transparent outline-none w-full"
 							/>
+
+							{/* Ícono de información */}
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="currentColor"
-								className="mr-[-10px] h-10"
+								className="mr-[-10px] h-10 cursor-pointer"
+								onClick={() => setShowExplanation(!showExplanation)}
+								title="Información sobre el formato del número"
 							>
 								<path
-									fill-rule="evenodd"
+									fillRule="evenodd"
 									d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-									clip-rule="evenodd"
+									clipRule="evenodd"
 								/>
 							</svg>
 						</div>
-						<div className="bg-black md:w-[500px]  shadow-black h-20  flex items-center justify-center absolute z-50 top-16 font-coolvetica rounded-lg px-4 left-4 right-4 md:left-auto md:right-auto">
-							<p className="text-center w-full text-sm text-gray-100 font-medium">
-								Ingresa tu número en el siguiente formato: 10 dígitos con el
-								codigo de area sin espacios ni símbolos.
-								<br />
-								Ej:<span className="font-bold"> 3585168971</span>
-							</p>
-						</div>
+
+						{/* Explicación del formato, renderizado condicionalmente */}
+						{showExplanation && (
+							<div className="bg-black md:w-[500px] shadow-black h-20 flex items-center justify-center absolute z-50 top-16 font-coolvetica rounded-lg px-4 left-4 right-4 md:left-auto md:right-auto">
+								<p className="text-center w-full text-sm text-gray-100 font-medium">
+									Ingresa tu número en el siguiente formato: 10 dígitos con el
+									código de área sin espacios ni símbolos.
+									<br />
+									Ej:<span className="font-bold"> 3585168971</span>
+								</p>
+							</div>
+						)}
 					</div>
 					<Carrusel />
 					<div className="top-[215px] inset-0 absolute">
