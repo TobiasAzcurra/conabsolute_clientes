81d269335ff87a4036aa0f30d6bfb42fadaac928
Tobías <tobiasazcurrabi@gmail.com>
2024-10-20 19:01:36 -0300

    fdasf

--- src/pages/pedido/Pedido.jsx
+++ src/pages/pedido/Pedido.jsx
@@ -402,24 +402,27 @@ const Pedido = () => {
 										</div>
 									</div>
 								</div>
-								<div
-									onClick={() => handleCancelClick(currentOrder.id)}
-									className={`${
-										isDeleting
-											? "opacity-50 cursor-not-allowed"
-											: "cursor-pointer"
-									} bg-gray-300 w-full text-red-main font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-12 font-bold`}
-								>
-									{isDeleting ? (
-										<div className="flex items-center justify-center space-x-2">
-											<LoadingPoints className="h-4 w-4" />
-										</div>
-									) : (
-										"Cancelar pedido"
-									)}
-								</div>
-								{/* Tu calificacion */}
-								{/* <div
+								{/* Botones */}
+								<div className="w-full">
+									{/* Cancelar pedido */}
+									<div
+										onClick={() => handleCancelClick(currentOrder.id)}
+										className={`${
+											isDeleting
+												? "opacity-50 cursor-not-allowed"
+												: "cursor-pointer"
+										} bg-gray-300 w-full text-red-main font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-12 font-bold`}
+									>
+										{isDeleting ? (
+											<div className="flex items-center justify-center space-x-2">
+												<LoadingPoints className="h-4 w-4" />
+											</div>
+										) : (
+											"Cancelar pedido"
+										)}
+									</div>
+									{/* Tu calificacion */}
+									{/* <div
 									onClick={() => handleRateClick(currentOrder.id)}
 									className="bg-gray-300 w-full text-black font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-4 font-bold cursor-pointer"
 								>
@@ -429,37 +432,41 @@ const Pedido = () => {
 										  } estrellas`
 										: "Calificar pedido"}
 								</div> */}
-								{/* Nuevo botón "Llamar cadete" */}
-								{currentOrder.cadete !== "NO ASIGNADO" && (
-									<div
-										onClick={() =>
-											window.open(`tel:${currentOrder.telefonoCadete}`, "_self")
-										} // Cambiar a la funcionalidad de llamada
-										className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-4 font-bold cursor-pointer"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											fill="currentColor"
-											className="h-6 mr-2"
+									{/* Llamar cadete */}
+									{currentOrder.cadete !== "NO ASIGNADO" && (
+										<div
+											onClick={() =>
+												window.open(
+													`tel:${currentOrder.telefonoCadete}`,
+													"_self"
+												)
+											} // Cambiar a la funcionalidad de llamada
+											className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-2 font-bold cursor-pointer"
 										>
-											<path
-												fillRule="evenodd"
-												d="M15 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0V5.56l-4.72 4.72a.75.75 0 1 1-1.06-1.06l4.72-4.72h-2.69a.75.75 0 0 1-.75-.75Z"
-												clipRule="evenodd"
-											/>
-											<path
-												fillRule="evenodd"
-												d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
-												clipRule="evenodd"
-											/>
-										</svg>
-										Llamar cadete
-									</div>
-								)}
-								{index < pedidosPagados.length - 1 && (
-									<div className="w-full h-px bg-black opacity-20 mt-8"></div>
-								)}
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 24 24"
+												fill="currentColor"
+												className="h-5 mr-2"
+											>
+												<path
+													fillRule="evenodd"
+													d="M15 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0V5.56l-4.72 4.72a.75.75 0 1 1-1.06-1.06l4.72-4.72h-2.69a.75.75 0 0 1-.75-.75Z"
+													clipRule="evenodd"
+												/>
+												<path
+													fillRule="evenodd"
+													d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+													clipRule="evenodd"
+												/>
+											</svg>
+											Llamar cadete
+										</div>
+									)}
+									{index < pedidosPagados.length - 1 && (
+										<div className="w-full h-px bg-black opacity-20 mt-8"></div>
+									)}
+								</div>
 							</div>
 						))}
 					</div>
