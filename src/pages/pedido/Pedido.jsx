2a7352f6758203579a849b43d493c2e2c97309b4
Tobías <tobiasazcurrabi@gmail.com>
2024-10-22 23:14:10 -0300

    dfasfa

--- src/pages/pedido/Pedido.jsx
+++ src/pages/pedido/Pedido.jsx
@@ -663,6 +663,55 @@ const Pedido = () => {
 										</div>
 										{/* Botones */}
 										<div className="w-full">
+											{/* Botón condicional: Llamar cadete o Escribir a soporte */}
+											{showSupportButton ? (
+												<div
+													onClick={handleSupportClick}
+													className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-12  font-bold cursor-pointer transition-colors duration-300"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														viewBox="0 0 24 24"
+														fill="currentColor"
+														className="h-5 mr-2"
+													>
+														<path
+															fillRule="evenodd"
+															d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
+															clipRule="evenodd"
+														/>
+													</svg>
+													Escribir a soporte
+												</div>
+											) : (
+												currentOrder.cadete !== "NO ASIGNADO" && (
+													<div
+														onClick={() =>
+															handleCadeteCall(currentOrder.cadete)
+														}
+														className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-12  font-bold cursor-pointer transition-colors duration-300"
+													>
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 24 24"
+															fill="currentColor"
+															className="h-5 mr-2"
+														>
+															<path
+																fillRule="evenodd"
+																d="M15 3.75a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0V5.56l-4.72 4.72a.75.75 0 11-1.06-1.06l4.72-4.72h-2.69a.75.75 0 01-.75-.75z"
+																clipRule="evenodd"
+															/>
+															<path
+																fillRule="evenodd"
+																d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+																clipRule="evenodd"
+															/>
+														</svg>
+														Llamar cadete
+													</div>
+												)
+											)}
 											{/* Cancelar pedido */}
 											<div
 												onClick={() => handleCancelClick(currentOrder.id)}
@@ -670,7 +719,7 @@ const Pedido = () => {
 													isDeleting
 														? "opacity-50 cursor-not-allowed"
 														: "cursor-pointer"
-												} bg-gray-300 w-full text-red-main font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-12 font-bold`}
+												} bg-gray-300 w-full text-red-main font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-2 font-bold`}
 											>
 												{isDeleting ? (
 													<div className="flex items-center justify-center space-x-2">
@@ -694,56 +743,6 @@ const Pedido = () => {
 													</div>
 												)}
 											</div>
-
-											{/* Botón condicional: Llamar cadete o Escribir a soporte */}
-											{showSupportButton ? (
-												<div
-													onClick={handleSupportClick}
-													className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-2 font-bold cursor-pointer transition-colors duration-300"
-												>
-													<svg
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 24 24"
-														fill="currentColor"
-														className="h-5 mr-2"
-													>
-														<path
-															fillRule="evenodd"
-															d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
-															clipRule="evenodd"
-														/>
-													</svg>
-													Escribir a soporte
-												</div>
-											) : (
-												currentOrder.cadete !== "NO ASIGNADO" && (
-													<div
-														onClick={() =>
-															handleCadeteCall(currentOrder.cadete)
-														}
-														className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl mt-2 font-bold cursor-pointer transition-colors duration-300"
-													>
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															viewBox="0 0 24 24"
-															fill="currentColor"
-															className="h-5 mr-2"
-														>
-															<path
-																fillRule="evenodd"
-																d="M15 3.75a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0V5.56l-4.72 4.72a.75.75 0 11-1.06-1.06l4.72-4.72h-2.69a.75.75 0 01-.75-.75z"
-																clipRule="evenodd"
-															/>
-															<path
-																fillRule="evenodd"
-																d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-																clipRule="evenodd"
-															/>
-														</svg>
-														Llamar cadete
-													</div>
-												)
-											)}
 
 											<AppleModal
 												isOpen={
