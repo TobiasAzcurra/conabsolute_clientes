5df7242410bd27b1437bfe62ba913063e6849a61
Tobías <tobiasazcurrabi@gmail.com>
2024-12-27 19:12:43 -0300

    fdas

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -12,6 +12,7 @@ import { canjearVoucher } from "../../firebase/validateVoucher";
 import Payment from "../mercadopago/Payment";
 import currencyFormat from "../../helpers/currencyFormat";
 import { calculateDiscountedTotal } from "../../helpers/currencyFormat";
+import isologo from "../../assets/isologo.png";
 import {
 	isWithinClosedDays,
 	isWithinOrderTimeRange,
@@ -552,7 +553,7 @@ const FormCustom = ({ cart, total }) => {
 														fill="currentColor"
 													/>
 												</svg>
-												Envienmelo
+												Delivery
 											</button>
 											<button
 												type="button"
@@ -565,19 +566,8 @@ const FormCustom = ({ cart, total }) => {
 													setFieldValue("deliveryMethod", "takeaway")
 												}
 											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													viewBox="0 0 24 24"
-													fill="currentColor"
-													class="h-6"
-												>
-													<path
-														fill-rule="evenodd"
-														d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
-														clip-rule="evenodd"
-													/>
-												</svg>
-												Retiro por local
+												<img src={isologo} className="h-6" alt="" />
+												Retiro
 											</button>
 										</div>
 										<div className="w-full items-center rounded-3xl border-2 border-black">
