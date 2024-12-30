0f2b782f1015d0f313ab6797e41e43592b3d7c64
Tobías <tobiasazcurrabi@gmail.com>
2024-12-29 21:44:16 -0300

    fdas

--- src/pages/pedido/UpdatedPedidoSection.jsx
+++ src/pages/pedido/UpdatedPedidoSection.jsx
@@ -162,7 +162,7 @@ const UpdatedPedidoSection = ({
 					{/* Agrega mas items al pedido */}
 					<div className="flex justify-center flex-col mt-6 items-start w-full">
 						<p className="text-xl font-bold text-center mx-auto mb-4">
-							Orgullese a Anhelo, agrega algo:
+							Enorgullese a Anhelo, agrega algo:
 						</p>
 						<div
 							className="flex gap-2 overflow-x-auto overflow-y-hidden custom-scrollbar"
