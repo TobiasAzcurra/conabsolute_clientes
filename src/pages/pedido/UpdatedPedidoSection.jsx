74ebe8a9cda71b6cba1163d07ff3ea1cc5b0f9c7
Tobías <tobiasazcurrabi@gmail.com>
2024-12-29 23:58:34 -0300

    sadfa

--- src/pages/pedido/UpdatedPedidoSection.jsx
+++ src/pages/pedido/UpdatedPedidoSection.jsx
@@ -99,31 +99,35 @@ const UpdatedPedidoSection = ({
 		<div className="flex flex-col font-coolvetica w-full">
 			{/* Pregunta */}
 			<div
-				className="w-full mt-2 gap-2 flex-row flex cursor-pointer"
+				className="w-full mt-2   flex-col flex cursor-pointer"
 				onClick={toggleModifyOrder}
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					className="h-6"
-				>
-					<path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
-					<path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
-				</svg>
-
-				<p className="font-bold  text-left">
-					Todavia podes modificar tu pedido
-				</p>
-				<div className="flex items-center">
-					<img
-						src={arrow}
-						className={`h-2 transition-transform duration-300 ${
-							isModifyOrderExpanded ? "rotate-180" : ""
-						}`}
-						alt=""
-					/>
+				<div className="flex flex-row   gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="h-6"
+					>
+						<path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+						<path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+					</svg>
+					<div className="flex flex-col  ">
+						<p className="font-bold  text-left">
+							Todavia podes modificar tu pedido,
+							<br /> cuando se cocine esta opcion desaparece.
+						</p>
+					</div>
 				</div>
+			</div>
+			<div className="w-full justify-center  flex">
+				<img
+					src={arrow}
+					className={`h-2 w-1.5 mt-4  transition-transform duration-300 ${
+						isModifyOrderExpanded ? "-rotate-90" : "rotate-90"
+					}`}
+					alt=""
+				/>
 			</div>
 
 			{/* Detalle del pedido y Agregar más items - Solo visible cuando isModifyOrderExpanded es true */}
