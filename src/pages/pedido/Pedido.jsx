730bfe094fa754d81147ba8d30c6455c350a8dff
Tobías <tobiasazcurrabi@gmail.com>
2024-12-29 23:18:32 -0300

    adfa

--- src/pages/pedido/Pedido.jsx
+++ src/pages/pedido/Pedido.jsx
@@ -10,6 +10,7 @@ import StickerCanvas from "../../components/StickerCanvas";
 import LoadingPoints from "../../components/LoadingPoints";
 import UpdatedPedidoSection from "./UpdatedPedidoSection";
 import AppleModal from "../../components/AppleModal";
+import isologo from "../../assets/isologo.png";
 import {
 	getCadetePhone,
 	updateRatingForOrder,
@@ -739,26 +740,40 @@ const Pedido = () => {
 													</div>
 												)}
 												<div className="flex flex-row gap-2">
-													<svg
-														xmlns="http://www.w3.org/2000/svg"
-														viewBox="0 0 24 24"
-														fill="currentColor"
-														className="h-6"
-													>
-														<path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
-														<path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
-													</svg>
+													{currentOrder.direccion === "" ? (
+														<img
+															src={isologo}
+															className="h-6 brightness-0"
+															alt=""
+														/>
+													) : (
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 24 24"
+															fill="currentColor"
+															className="h-6"
+														>
+															<path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+															<path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+														</svg>
+													)}
+
 													<p
 														className="text-black font-coolvetica font-medium cursor-pointer"
 														onClick={() => setShowFullAddress(!showFullAddress)}
 													>
-														Destino:{" "}
-														{currentOrder.direccion === ""
-															? "Retirar por Buenos Aires 618"
-															: showFullAddress
-															? currentOrder.direccion
-															: (currentOrder.direccion?.split(",")[0].trim() ||
-																	"No disponible") + "..."}
+														{currentOrder.direccion === "" ? (
+															"Retirar por Buenos Aires 618"
+														) : (
+															<>
+																Destino:{" "}
+																{showFullAddress
+																	? currentOrder.direccion
+																	: (currentOrder.direccion
+																			?.split(",")[0]
+																			.trim() || "No disponible") + "..."}
+															</>
+														)}
 													</p>
 												</div>
 												<div className="flex flex-row gap-2">
