55029839428fbeab663853abd30aa9b5d7875b69
Tobías <tobiasazcurrabi@gmail.com>
2024-12-29 21:47:35 -0300

    interfaz

--- src/pages/pedido/UpdatedPedidoSection.jsx
+++ src/pages/pedido/UpdatedPedidoSection.jsx
@@ -89,42 +89,41 @@ const UpdatedPedidoSection = ({
 	};
 
 	return (
-		<div className="flex flex-col font-coolvetica w-full">
+		<div className="flex flex-col justify-center  items-center font-coolvetica w-full">
 			{/* Pregunta */}
 			<div
-				className="w-full mt-2 flex-col px-4 flex cursor-pointer"
+				className="w-full mt-6 flex-col  px-4 flex cursor-pointer"
 				onClick={toggleModifyOrder}
 			>
-				<div className="flex flex-row gap-2">
-					<svg
+				<div className="flex flex-col justify-center items-center">
+					{/* <svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
 						fill="currentColor"
-						className="h-6"
+						className="h-6 "
 					>
 						<path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
 						<path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
-					</svg>
-					<div className="flex flex-col">
-						<p className="font-bold text-left">
-							Podes agregar productos a tu pedido!
-							<br /> Por logistica las modificaciones son limitadas y cuando se
-							cocine esta opcion desaparece!
-							<br />
+					</svg> */}
+					<p className="font-bold text-center">
+						¡Podes agregar productos a tu pedido!
+						<br />
+						<p className="text-xs font-medium">
+							Por logistica las modificaciones son limitadas y cuando se cocine{" "}
+							<br /> esta opcion desaparece.
 						</p>
-					</div>
+						<br />
+					</p>
 				</div>
 			</div>
 
-			<div className="w-full justify-center px-4 flex">
-				<img
-					src={arrow}
-					className={`h-2 w-1.5 mt-4 transform ${
-						isModifyOrderExpanded ? "-rotate-90" : "arrow-bounce"
-					}`}
-					alt=""
-				/>
-			</div>
+			<img
+				src={arrow}
+				className={`h-2 w-1.5  transform ${
+					isModifyOrderExpanded ? "-rotate-90" : "arrow-bounce"
+				}`}
+				alt=""
+			/>
 
 			{/* Expanded content */}
 			{isModifyOrderExpanded && (
