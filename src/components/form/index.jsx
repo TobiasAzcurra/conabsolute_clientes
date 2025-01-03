c4233e54fc27f914181c9933044fa694be495d4b
Tobías <tobiasazcurrabi@gmail.com>
2025-01-03 12:43:44 -0300

    Fastest posible

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -957,7 +957,7 @@ const FormCustom = ({ cart, total }) => {
 												/>
 											</svg>
 											<p className="font-coolvetica flex flex-row items-center gap-1">
-												Envio express
+												Lo mas rapido posible
 												<p className="font-bold ">(+$500)</p>
 											</p>
 										</div>
@@ -968,7 +968,21 @@ const FormCustom = ({ cart, total }) => {
 										<p>{currencyFormat(total)}</p>
 									</div>
 									<div className="flex flex-row justify-between w-full">
-										<p className="font-coolvetica font-medium">Envío</p>
+										<div className="flex flex-row items-center gap-2">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 24 24"
+												fill="currentColor"
+												className="h-4 text-black"
+											>
+												<path
+													fillRule="evenodd"
+													d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+													clipRule="evenodd"
+												/>
+											</svg>
+											<p className="font-coolvetica font-medium">Envío</p>
+										</div>
 										<p>
 											{values.deliveryMethod === "delivery"
 												? currencyFormat(envio)
@@ -976,7 +990,9 @@ const FormCustom = ({ cart, total }) => {
 										</p>
 									</div>
 									<div className="flex flex-row justify-between w-full">
-										<p className="font-coolvetica font-medium">Express</p>
+										<p className="font-coolvetica font-medium">
+											Velocidad extra
+										</p>
 										<p>
 											{values.deliveryMethod === "delivery" && isEnabled
 												? currencyFormat(expressDeliveryFee)
