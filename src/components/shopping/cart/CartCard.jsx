2f0e67aba0a1263d28c400ff094c9884c1672960
Tobías <tobiasazcurrabi@gmail.com>
2025-01-05 20:37:32 -0300

    Mejora de interfaz

--- src/components/shopping/cart/CartCard.jsx
+++ src/components/shopping/cart/CartCard.jsx
@@ -140,7 +140,11 @@ const CartCard = ({
 
 				<div className="flex flex-col w-2/3 justify-center px-4 pt-2 pb-4">
 					<div>
-						<h3 className="text-2xl font-bold mb-1.5 leading-6">
+						<h3
+							className={`text-2xl font-bold mb-1.5 ${
+								showConfirmation ? "truncate leading-none" : "leading-6"
+							}`}
+						>
 							{capitalizeWords(name)}
 						</h3>
 						{toppings && toppings.length > 0 && (
