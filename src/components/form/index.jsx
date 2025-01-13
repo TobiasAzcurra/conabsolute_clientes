d73d153ba13dcf36bccf5ccc8198f92ff502d41b
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-01-12 21:22:13 -0300

    tooltip

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -850,11 +850,15 @@ const FormCustom = ({ cart, total }) => {
   )}
 </Field>
  
-  {!isOpenPaymentMethod && (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-4 absolute text-red-main right-8">
-    <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clipRule="evenodd" />
-  </svg>
-)}
+{!isOpenPaymentMethod && (
+    <div className="absolute right-8">
+      <Tooltip
+      position="left"
+        text="Clickea en pedir para ver una mejor explicacion de por que no te deja elegir otra opcion."
+        duration={5000}
+        className="cursor-pointer"/>
+    </div>
+  )}
 </div>
                     </div>
 
