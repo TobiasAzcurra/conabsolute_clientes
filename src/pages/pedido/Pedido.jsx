04f582ddaf3ce8abb18f109e8d3e5047266a8472
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-09 13:40:05 -0300

    obtener compensacion boton

--- src/pages/pedido/Pedido.jsx
+++ src/pages/pedido/Pedido.jsx
@@ -338,6 +338,15 @@ const Pedido = () => {
         const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
         window.open(whatsappUrl, "_blank");
     };
+
+    const handleCompensationClick = (message) => {
+        console.log("💬 Iniciando contacto por compensación");
+        const phoneNumber = "543584306832";
+        const whatsappMessage = `Hola! Acepte mi pedido con esta condicion: ${message}, cual es mi compensacion?`;
+        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+        window.open(whatsappUrl, "_blank");
+    };
+
     const handleEditAddress = (orderId) => {
         setEditingOrderId(orderId);
         setIsEditAddressModalOpen(true);
@@ -826,8 +835,27 @@ const Pedido = () => {
                                             </div></div>
                                         {/* botones */}
                                         <div className={`w-full px-4 ${hasButtons ? "mt-11" : "mt-4"}`}>
+
+                                            {/* condicion*/}
+                                            {currentOrder.message && (
+                                                <div
+                                                    className="bg-gray-300 w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-green-500 text-2xl rounded-3xl font-bold cursor-pointer transition-colors duration-300 mb-2"
+                                                    onClick={() => handleCompensationClick(currentOrder.message)}
+                                                >
+                                                    <div className="flex items-center">
+
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 mr-2">>
+                                                            <path fill-rule="evenodd" d="M12 1.5c-1.921 0-3.816.111-5.68.327-1.497.174-2.57 1.46-2.57 2.93V21.75a.75.75 0 0 0 1.029.696l3.471-1.388 3.472 1.388a.75.75 0 0 0 .556 0l3.472-1.388 3.471 1.388a.75.75 0 0 0 1.029-.696V4.757c0-1.47-1.073-2.756-2.57-2.93A49.255 49.255 0 0 0 12 1.5Zm-.97 6.53a.75.75 0 1 0-1.06-1.06L7.72 9.22a.75.75 0 0 0 0 1.06l2.25 2.25a.75.75 0 1 0 1.06-1.06l-.97-.97h3.065a1.875 1.875 0 0 1 0 3.75H12a.75.75 0 0 0 0 1.5h1.125a3.375 3.375 0 1 0 0-6.75h-3.064l.97-.97Z" clip-rule="evenodd" />
+                                                        </svg>
+
+                                                        Obtener compensación
+                                                    </div>
+                                                </div>
+                                            )}
+
+                                            {/* pago virtual */}
                                             <div
-                                                className="bg-black w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl font-bold cursor-pointer transition-colors duration-300"
+                                                className="bg-gray-300 text-blue-500 w-full text-gray-100 font-coolvetica text-center justify-center h-20 flex items-center text-2xl rounded-3xl font-bold cursor-pointer transition-colors duration-300"
                                                 onClick={() => handleTransferenciaClick(currentOrder.total, currentOrder.telefono)}
 
 
@@ -842,6 +870,8 @@ const Pedido = () => {
                                                     Pagar virtualmente
                                                 </div>
                                             </div>
+
+                                            {/* soporte */}
                                             {showSupportButton && (
                                                 <div
                                                     onClick={handleSupportClick}
@@ -862,6 +892,8 @@ const Pedido = () => {
                                                     Escribir a soporte
                                                 </div>
                                             )}
+
+                                            {/* cancelar */}
                                             {showCancelButton && (
                                                 <div
                                                     onClick={() => handleCancelClick(currentOrder.id)}
