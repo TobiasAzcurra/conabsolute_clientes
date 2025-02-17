65d3dad983c588a4095c72fe9665ba802cb47476
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-17 17:35:46 -0300

    gfsd

--- src/pages/Reclamos.tsx
+++ src/pages/Reclamos.tsx
@@ -265,7 +265,7 @@ const Reclamos = () => {
                     {renderOrdersSection()}
 
                     {selectedOrder && (
-                        <div className="flex flex-col px-4 space-y-2">
+                        <div className="flex flex-col px-4 ">
                             <input
                                 name="descripcion"
                                 value={formData.descripcion}
@@ -278,13 +278,13 @@ const Reclamos = () => {
                             <button
                                 type="submit"
                                 disabled={loading || !selectedOrder}
-                                className={`w-full bg-black text-white font-coolvetica text-2xl h-20 rounded-3xl font-bold 
+                                className={`w-full bg-black text-gray-100 font-coolvetica text-2xl h-20 rounded-3xl font-bold 
                                 ${(loading || !selectedOrder) ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-900'} 
                                 transition-colors duration-200 mt-8 flex items-center justify-center gap-2`}
                             >
                                 {loading ? (
                                     <>
-                                        <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <svg className="animate-spin h-5 w-5 text-gray-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                                         </svg>
