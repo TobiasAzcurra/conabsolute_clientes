08026c3fafa50b3cba87c62ed53417def24f7e50
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-17 17:58:48 -0300

    gfds

--- src/pages/Reclamos.tsx
+++ src/pages/Reclamos.tsx
@@ -232,6 +232,13 @@ const Reclamos = () => {
                 <p className=" text-sm text-center text-gray-600">Damos lo mejor de nosotros pero a veces hay cosas que escapan de nuestro control y nos responsabilizamos cuando ocurre! Es nuestro deber con la gente que nos elige ❤️‍🩹
                     <br />En breve te transfieren la compensacion y aca van unos regalos! Esperamos volver a verte!
                 </p>
+                <div className='text-4xl w-full z-50 text-center mt-6 flex items-center justify-center bg-red-main text-gray-100 rounded-3xl h-20 font-bold'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 mr-2">
+                        <path d="M9.375 3a1.875 1.875 0 0 0 0 3.75h1.875v4.5H3.375A1.875 1.875 0 0 1 1.5 9.375v-.75c0-1.036.84-1.875 1.875-1.875h3.193A3.375 3.375 0 0 1 12 2.753a3.375 3.375 0 0 1 5.432 3.997h3.943c1.035 0 1.875.84 1.875 1.875v.75c0 1.036-.84 1.875-1.875 1.875H12.75v-4.5h1.875a1.875 1.875 0 1 0-1.875-1.875V6.75h-1.5V4.875C11.25 3.839 10.41 3 9.375 3ZM11.25 12.75H3v6.75a2.25 2.25 0 0 0 2.25 2.25h6v-9ZM12.75 12.75v9h6.75a2.25 2.25 0 0 0 2.25-2.25v-6.75h-9Z" />
+                    </svg>
+                    Regalo
+
+                </div>
             </div>
         );
     }
