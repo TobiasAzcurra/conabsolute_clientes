f68f28921fc465ad78e6ae4c8f313e15d2af0b0a
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-17 16:53:16 -0300

    fds

--- src/pages/Reclamos.tsx
+++ src/pages/Reclamos.tsx
@@ -197,9 +197,16 @@ const Reclamos = () => {
                                                 e.stopPropagation();
                                                 toggleOrderDetails(order.id, e);
                                             }}
-                                            className="text-sm text-gray-600 font-medium"
+                                            className="text-sm text-gray-600 flex flex-row gap-1 font-medium items-center"
                                         >
-                                            {expandedOrders.has(order.id) ? 'Ocultar pedido' : 'Ver pedido'}
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-4">
+                                                <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clip-rule="evenodd" />
+                                            </svg>
+
+                                            <p>
+
+                                                {expandedOrders.has(order.id) ? 'Ocultar pedido' : 'Ver pedido'}
+                                            </p>
                                         </button>
                                         {expandedOrders.has(order.id) && (
                                             <ul className="flex justify-center flex-col items-left mt-2">
