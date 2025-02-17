c552e3c3688978bd972e179ffa88c622997519b3
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-17 17:38:41 -0300

    sfdg

--- src/pages/Reclamos.tsx
+++ src/pages/Reclamos.tsx
@@ -169,7 +169,7 @@ const Reclamos = () => {
                         <img src={arrow} className='h-2 rotate-180 opacity-60' alt="" />
                         Volver
                     </button>
-                    <h3 className="font-coolvetica text-sm mt-4 text-center">
+                    <h3 className={`font-coolvetica text-sm text-center ${selectedOrder ? '' : 'mt-4'}`}>
                         {selectedOrder ? 'Pedido seleccionado:' : 'Selecciona el pedido fallido:'}
                     </h3>
                 </div>
