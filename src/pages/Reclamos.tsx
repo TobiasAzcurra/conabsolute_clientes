9e482c73f05be4ee7e203a78b4ebdcde4990867b
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-18 11:37:12 -0300

    fdas

--- src/pages/Reclamos.tsx
+++ src/pages/Reclamos.tsx
@@ -275,10 +275,25 @@ const Reclamos = () => {
                                 type="button"
                                 onClick={handleSearch}
                                 disabled={searching || formData.telefono.length < 8}
-                                className=" h-10 w-20 flex items-center justify-center border-l border-black rounded-r-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5">
-                                    <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
-                                </svg>
+                                className=" h-10 w-20 bg-black flex items-center justify-center border-l border-black rounded-r-full">
+                                {searching ? (
+                                    <LoadingPoints color='text-gray-100' />
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                        className="h-5 text-gray-100"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                )}
+
+
                             </button>
                         </div>
                     )}
