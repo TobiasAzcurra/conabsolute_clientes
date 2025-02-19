8dfc466886eb732622bd443b88a8c1f36676b317
TobiasAzcurra <tobiascommonplace@gmail.com>
2025-02-19 14:37:15 -0300

    fdas

--- src/Router.jsx
+++ src/Router.jsx
@@ -276,10 +276,10 @@ const AppRouter = () => {
 											onClick={handleSuggestionClick}
 											className="w-full font-bold text-left px-4 h-10 rounded-md text-xs flex items-center "
 										>
-											<svg xmlns="http://www.w3.org/2000/svg" className="h-4 text-gray-400 mr-1" viewBox="0 0 24 24" fill="currentColor">
+											<svg xmlns="http://www.w3.org/2000/svg" className="h-4 text-0 mr-1" viewBox="0 0 24 24" fill="currentColor">
 												<path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clipRule="evenodd" />
 											</svg>
-											<p className='text-gray-400 text-xs font-light  mr-2'>
+											<p className='text text-xs font-light  mr-2'>
 												Ultimo pedido:
 											</p>
 											<p className=" text-sm">
