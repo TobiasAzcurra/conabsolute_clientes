5818e90ac65b347dba5409cd0cb13d544907313b
TorlettiJoaquin <axeltorletti@gmail.com>
2025-06-16 16:59:00 -0300

    Agregar manejo de datos del cliente y optimizar la visualización del layout

--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useParams } from 'react-router-dom';
+import useClientData from '../hooks/useClientData';
 import Carrusel from '../components/Carrusel';
 import NavMenu from '../components/NavMenu';
 import FloatingCart from '../components/shopping/FloatingCart';
 import { useSelector } from 'react-redux';
 import logo from '../assets/Logo APM-07.png';
 import SearchBar from '../components/SearchBar';
+import { useLocation } from 'react-router-dom';
 
 const ClientLayout = ({
   children,
@@ -12,6 +16,18 @@ const ClientLayout = ({
   selectedItem,
   introConfig,
 }) => {
+  const { slug } = useParams();
+  const { clientData } = useClientData(slug);
+
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const isProductDetail = /\/menu\/[^/]+\/[^/]+$/.test(pathname);
+  const isCart = pathname.endsWith('/carrito');
+
+  const shouldHideHeader = isProductDetail || isCart;
+  const shouldShowFloatingCart = !isCart;
+
   const cart = useSelector((state) => state.cartState.cart);
   const totalQuantity = cart.reduce((acc, item) => acc + item.quantity, 0);
 
@@ -42,42 +58,63 @@ const ClientLayout = ({
   };
 
   return (
-    <div className="flex flex-col relative mb-[90px]">
-      <SearchBar
-        phoneNumber={phoneNumber}
-        setPhoneNumber={setPhoneNumber}
-        showSuggestion={showSuggestion}
-        setShowSuggestion={setShowSuggestion}
-        previousPhone={previousPhone}
-        onSuggestionClick={onSuggestionClick}
-      />
-
-      <img
-        src={logo}
-        className="h-20 top-[100px] left-1/2 -translate-x-1/2 absolute z-[30]"
-        alt="Logo"
-      />
-
-      <div className="relative z-[10]">
-        <Carrusel images={introConfig.hero || []} />
-
-        <div className="top-[215px] inset-0 absolute">
-          <NavMenu
-            selectedItem={selectedItem}
-            handleItemClick={handleItemClick}
-          />
-        </div>
-      </div>
-
-      <div className="mt-[100px] z-[5]">{children}</div>
-
-      {totalQuantity > 0 && (
-        <>
-          <div className="fixed inset-x-0 bottom-0 z-50 h-12 bg-gradient-to-t from-black/50 to-transparent pointer-events-none backdrop-blur-sm" />
-          <FloatingCart totalQuantity={totalQuantity} cart={cart} />
-        </>
+    <>
+      {clientData && (
+        <Helmet>
+          <title>{clientData?.name || 'ABSOLUTE'}</title>
+          {clientData?.logo && (
+            <link
+              rel="icon"
+              type="image/png"
+              href={clientData.logo}
+              key={`favicon-${clientData.logo}`}
+            />
+          )}
+        </Helmet>
       )}
-    </div>
+      <div className="flex flex-col relative mb-[90px]">
+        {!shouldHideHeader && (
+          <>
+            <div className="fixed inset-x-0 top-0 z-50 h-10 bg-gradient-to-b from-black/50 to-transparent pointer-events-none backdrop-blur-sm" />
+            <SearchBar
+              phoneNumber={phoneNumber}
+              setPhoneNumber={setPhoneNumber}
+              showSuggestion={showSuggestion}
+              setShowSuggestion={setShowSuggestion}
+              previousPhone={previousPhone}
+              onSuggestionClick={onSuggestionClick}
+            />
+
+            <img
+              src={logo}
+              className="h-20 top-[100px] left-1/2 -translate-x-1/2 absolute z-[30]"
+              alt="Logo"
+            />
+
+            <div className="relative z-[10]">
+              <Carrusel images={introConfig.hero || []} />
+              <div className="top-[215px] inset-0 absolute">
+                <NavMenu
+                  selectedItem={selectedItem}
+                  handleItemClick={handleItemClick}
+                />
+              </div>
+            </div>
+          </>
+        )}
+
+        <div className={`${!shouldHideHeader ? 'mt-[100px]' : ''} z-[5]`}>
+          {children}
+        </div>
+
+        {shouldShowFloatingCart && totalQuantity > 0 && (
+          <>
+            <div className="fixed inset-x-0 bottom-0 z-40 h-12 bg-gradient-to-t from-black/50 to-transparent pointer-events-none backdrop-blur-sm" />
+            <FloatingCart totalQuantity={totalQuantity} cart={cart} />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
