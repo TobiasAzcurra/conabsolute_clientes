de00a7316c73dfd7a60ed54671e718f62ccb2180
TorlettiJoaquin <axeltorletti@gmail.com>
2025-06-30 03:16:16 -0300

    Refactor listenOrdersByPhone.js para simplificar la escucha de pedidos por teléfono y mejorar la legibilidad del código

--- src/firebase/orders/listenOrdersByPhone.js
+++ src/firebase/orders/listenOrdersByPhone.js
@@ -1,64 +1,47 @@
-import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
-import { useClient } from '../../contexts/ClientContext';
-import { obtenerFechaActual } from '../utils/dateHelpers';
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+} from 'firebase/firestore';
+import { app } from '../config/firebaseConfig';
 
-export const ListenOrdersForTodayByPhoneNumber = (phoneNumber, callback) => {
-  const firestore = getFirestore();
-  const { slugEmpresa, slugSucursal } = useClient();
-  const todayDateString = obtenerFechaActual();
+const db = getFirestore(app);
 
-  // Obtener el año, mes y día actual
-  const [day, month, year] = todayDateString.split('/');
-
-  // Referencia al documento del día actual dentro de la colección del mes actual
-  const ordersDocRef = doc(
-    firestore,
+export const listenOrdersByPhone = (
+  empresaId,
+  sucursalId,
+  phoneNumber,
+  callback
+) => {
+  const pedidosCollectionRef = collection(
+    db,
     'absoluteClientes',
-    slugEmpresa,
+    empresaId,
     'sucursales',
-    slugSucursal,
-    'pedidos',
-    year,
-    month,
-    day
+    sucursalId,
+    'pedidos'
   );
 
-  // console.log(
-  // 	`📡 Escuchando pedidos para el número de teléfono ${phoneNumber} en la fecha ${day}/${month}/${year}`
-  // );
+  const pedidosQuery = query(
+    pedidosCollectionRef,
+    where('telefono', '==', phoneNumber),
+    where('canceled', '!=', true)
+  );
 
-  // Escuchar cambios en el documento del día actual
   return onSnapshot(
-    ordersDocRef,
-    (docSnapshot) => {
-      if (docSnapshot.exists()) {
-        const pedidosDelDia = docSnapshot.data()?.pedidos || [];
-
-        // Filtrar los pedidos por el número de teléfono
-        const pedidosFiltrados = pedidosDelDia.filter(
-          (pedido) => pedido.telefono === phoneNumber && !pedido.canceled
-        );
-
-        // console.log(
-        // 	`📦 Pedidos filtrados para el número ${phoneNumber}:`,
-        // 	pedidosFiltrados
-        // );
-
-        callback(pedidosFiltrados); // Devuelve un array de pedidos filtrados
-      } else {
-        // Si el documento no existe, no hay pedidos para el día actual
-        console.warn(
-          `⚠️ No existen pedidos para la fecha ${day}/${month}/${year}.`
-        );
-        callback([]); // Devuelve un array vacío
-      }
+    pedidosQuery,
+    (querySnapshot) => {
+      const pedidos = [];
+      querySnapshot.forEach((doc) => {
+        pedidos.push({ id: doc.id, ...doc.data() });
+      });
+      callback(pedidos);
     },
     (error) => {
-      console.error(
-        '❌ Error al escuchar los pedidos para el día actual:',
-        error
-      );
-      callback([]); // Devuelve un array vacío en caso de error
+      console.error('❌ Error al escuchar los pedidos por teléfono:', error);
+      callback([]);
     }
   );
 };
