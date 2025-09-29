41b18121baa2c04993f70a1f8534b12c868678c3
Tobías Azcura <tobiascommonplace@gmail.com>
2025-09-29 13:44:15 -0300

    dsa

--- src/components/shopping/card/Card.jsx
+++ src/components/shopping/card/Card.jsx
@@ -222,12 +222,12 @@ const Card = ({ data, path }) => {
             </div>
           )}
 
-          {imageError && (
+          {!currentImageSrc && (
             <div className="absolute inset-0 flex items-center justify-center bg-gray-300">
               <div className="text-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-12 w-12 text-gray-500 mx-auto mb-2"
+                  className="h-6 text-gray-400 mx-auto "
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -239,9 +239,6 @@ const Card = ({ data, path }) => {
                     d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
-                <span className="text-gray-400 font-light text-xs">
-                  Sin imagen
-                </span>
               </div>
             </div>
           )}
